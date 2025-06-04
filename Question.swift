4c5d5207959b7cc945f63eb2f54bb197d18522a3
50CalBarrett <alexander.barrett.f@gmail.com>
2025-06-04 13:25:17 -0500

    Create Question.swift

---
+++ Question.swift
@@ -0,0 +1,15 @@
+//
+//  Question.swift
+//  NetworkPlusNew
+//
+//  Created by NotBatman on 6/3/25.
+//
+
+import Foundation
+
+struct Question {
+    var text: String
+    var choices: [String]
+    var correctAnswers: [String]
+}
+
