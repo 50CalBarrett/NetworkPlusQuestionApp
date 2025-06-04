d5f5982c072f8a130ae2d9ff5cbb0316e5e64757
50CalBarrett <alexander.barrett.f@gmail.com>
2025-06-04 13:23:28 -0500

    Create NetworkPlusApp.swift

---
+++ NetworkPlusApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkPlusApp.swift
+//  NetworkPlusNew
+//
+//  Created by NotBatman on 6/3/25.
+//
+
+import SwiftUI
+
+@main
+struct YourAppNameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            WelcomeView()
+        }
+    }
+}
